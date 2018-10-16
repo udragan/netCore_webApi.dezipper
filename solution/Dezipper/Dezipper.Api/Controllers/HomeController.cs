15c01688dc19f69ead72d79be6efe9efffd5f2bc
udragan <ujfa2001@yahoo.com>
2018-10-16 18:56:48 +0200

    added default Home controller

---
+++ solution/Dezipper/Dezipper.Api/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dezipper.Api.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/[controller]")]
+	public class HomeController : Controller
+	{
+		// GET: api/Home
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET: api/Home/5
+		[HttpGet("{id}", Name = "Get")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST: api/Home
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/Home/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/ApiWithActions/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
