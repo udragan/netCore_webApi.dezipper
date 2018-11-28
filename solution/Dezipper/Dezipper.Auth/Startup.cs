4ea8ffddad1f2fdcf104f1c01f7db9f0d51a05fc
udragan <ujfa2001@yahoo.com>
2018-11-28 21:04:53 +0100

    added IdentityServer4 nuget reference

--- solution/Dezipper/Dezipper.Auth/Startup.cs
+++ solution/Dezipper/Dezipper.Auth/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace com.udragan.netCore.webApi.Dezipper.Auth
@@ -11,6 +10,8 @@ namespace com.udragan.netCore.webApi.Dezipper.Auth
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddIdentityServer()
+				.AddDeveloperSigningCredential();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,10 +22,7 @@ namespace com.udragan.netCore.webApi.Dezipper.Auth
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+			app.UseIdentityServer();
 		}
 	}
 }
